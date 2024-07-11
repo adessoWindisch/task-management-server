f4f4a83d931acdb35a88aa892c3202bc3c73432e
Windisch, Sascha <Sascha.Windisch@adesso.de>
2024-07-11 12:42:06 +0200

    Spring OpenAPI Doc added

--- src/main/java/de/adesso/pdd/taskmanagement/dto/EmployeeDto.java
+++ src/main/java/de/adesso/pdd/taskmanagement/dto/EmployeeDto.java
@@ -1,26 +1,33 @@
 package de.adesso.pdd.taskmanagement.dto;
 
-import lombok.Data;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
-
+import lombok.Data;
 
 @Data
+@Schema(description = "Employee Data Transfer Object")
 public class EmployeeDto {
+    @Schema(description = "Employee ID", example = "1", required = true)
     private Long employeeId;
 
     @NotEmpty(message = "OrgID can not be a null or empty!")
+    @Schema(description = "Organization ID", example = "A123", required = true)
     private String orgId;
 
     @NotEmpty(message = "First Name can not be a null or empty!")
+    @Schema(description = "Employee's first name", example = "John", required = true)
     private String firstName;
 
     @NotEmpty(message = "Last Name can not be a null or empty!")
+    @Schema(description = "Employee's last name", example = "Doe", required = true)
     private String lastName;
 
     @Pattern(regexp = "^(.+)@(.+)$", message = "Email should be valid!")
+    @Schema(description = "Employee's email address", example = "john.doe@example.com", required = true)
     private String eMail;
 
     @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits!")
+    @Schema(description = "Employee's mobile number", example = "1234567890", required = false)
     private String mobileNumber;
 }
